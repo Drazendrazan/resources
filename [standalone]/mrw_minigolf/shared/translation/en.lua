233b81594cc1005c2852dde4e0e7094fb21cf384
Krisb239 <118583895+Krisb239@users.noreply.github.com>
2022-11-20 11:25:02 -0500

    update

--- [standalone]/mrw_minigolf/shared/translation/en.lua
+++ [standalone]/mrw_minigolf/shared/translation/en.lua
@@ -1,17 +1,17 @@
 if language == "en" then
     translation = {
-        ["congrats"] = "Felicidades",
-        ["round_win"] = "Terminaste este hoyo con ",
-        ["finish_game"] = "Se acabo el juego, hiciste un total de : ",
-        ["stroke"] = "Tiro",
-        ["locate_club"] = "~INPUT_CONTEXT~ para rentar palos por $200 !",
-        ["other_params"] = "~INPUT_ATTACK~ para cargar el golpe !\n ~INPUT_VEH_FLY_ATTACK_CAMERA~ mostrar los puntos ! \n",
+        ["congrats"] = "Congratulations",
+        ["round_win"] = "Round Won",
+        ["finish_game"] = "Game Over",
+        ["stroke"] = "Stroke",
+        ["locate_club"] = "~INPUT_CONTEXT~ Start game for 200!",
+        ["other_params"] = "~INPUT_ATTACK~ para cargar el .golpe !\n ~INPUT_VEH_FLY_ATTACK_CAMERA~ mostrar los puntos ! \n",
         ["rotate_params"] = "~INPUT_CELLPHONE_LEFT~ ~INPUT_CELLPHONE_RIGHT~ rotar alrededor de la bola !\n",
         ["games_params"] = "~INPUT_VEH_PREV_RADIO~ para reiniciar el nivel !\n ~INPUT_CELLPHONE_OPTION~ quitar el juego !",
-        ["no_money"] = "No tienes fondos !",
-        ["max_stroke"] = "Superaste el maximo de golpes en este round",
-        ["quit"] = "Saliste del juego",
-        ["off_side"] = "La bola salio del campo",
-        ["play_your"] = "Total de Tiros: "
+        ["no_money"] = "Not enough money!",
+        ["max_stroke"] = "Reached Max Stokes per round",
+        ["quit"] = "Quit",
+        ["off_side"] = "Off sides",
+        ["play_your"] = "Your play:"
     }
 end
